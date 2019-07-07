f728e081cb5b2e1d41d46a6930c9918e7a29b38e
Emad Masoud <emad3000@gmail.com>
2019-07-07 04:53:32 +0000

    so far all function working

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,20 +1,45 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+
   def index
-    @places = Place.all
+  @places = Place.all
   end
 
   def new
-    @place = Place.new
+  @place = Place.new
   end
 
   def create
-    Place.create(place_params)
-    redirect_to root_path
+  current_user.places.create(place_params)
+  redirect_to root_path
+  end
+
+  def show
+  @place = Place.find(params[:id])
+  end
+
+  def edit
+  @place = Place.find(params[:id])
+  if @place.user != current_user
+  return render plain: 'Not Allowed', status: :forbidden
+  end
+  end
+
+  def update
+  @place = Place.find(params[:id])
+  @place.update_attributes(place_params)
+  redirect_to root_path
+  end
+
+  def destroy
+  @place = Place.find(params[:id])
+  @place.destroy
+  redirect_to root_path
   end
 
   private
 
   def place_params
-    params.require(:place).permit(:name, :description, :address)
+  params.require(:place).permit(:name, :description, :address)
   end
 end
