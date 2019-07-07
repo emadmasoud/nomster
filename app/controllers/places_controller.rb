8dadb016cbdf7e4b3eb84505ad6808fe3a3f4b30
Emad Masoud <emad3000@gmail.com>
2019-07-07 15:34:54 +0000

    gemile log switch

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
   @places = Place.all
   end
@@ -10,8 +9,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-  current_user.places.create(place_params)
+  @place = current_user.places.create(place_params)
+  if @place.valid?
   redirect_to root_path
+  else
+  render :new, status: :unprocessable_entity
+  end
   end
 
   def show
@@ -30,13 +33,21 @@ class PlacesController < ApplicationController
   if @place.user != current_user
   return render plain: 'Not Allowed', status: :forbidden
   end
+
   @place.update_attributes(place_params) 
-  
+  if @place.valid?
   redirect_to root_path
+  else
+  render :edit, status: :unprocessable_entity
+  end
   end
 
   def destroy
   @place = Place.find(params[:id])
+  if @place.user != current_user
+  return render plain: 'Not Allowed', status: :forbidden
+  end
+
   @place.destroy
   redirect_to root_path
   end
